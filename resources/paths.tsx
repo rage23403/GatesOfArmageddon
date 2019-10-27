22472ed43ea9b32236d56abdb84d87db80be9b6f
Derek Pulley <rage23403@gmail.com>
2019-10-27 14:52:10 -0500

    sprite sheets

---
+++ resources/paths.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.09.18" name="paths" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="Sprites/tileSheet.png" width="512" height="512"/>
+ <tile id="0" type="grass"/>
+ <tile id="1" type="dirt"/>
+ <tile id="2" type="dirt"/>
+ <tile id="3" type="grass"/>
+ <tile id="4" type="dirtC"/>
+ <tile id="5" type="pathC"/>
+ <tile id="6" type="pathC"/>
+ <tile id="7" type="dirtC"/>
+ <tile id="8" type="dirtC"/>
+ <tile id="9" type="dirtE"/>
+ <tile id="10" type="path"/>
+ <tile id="11" type="path"/>
+ <tile id="12" type="grass"/>
+ <tile id="13" type="pathC"/>
+ <tile id="14" type="dirtE"/>
+ <tile id="15" type="dirtE"/>
+ <tile id="16" type="dirtE"/>
+ <tile id="17" type="pathC"/>
+ <tile id="18" type="dirtC"/>
+</tileset>
